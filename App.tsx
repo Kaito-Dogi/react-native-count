5b42eb618bcd0ced30d232690f7b87367862ddd0
Kaito-Dogi <kendobu0405@gmail.com>
2023-01-14 01:00:51 +0900

    feat: カウント機能を実装

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import {useState} from "react";
 
 export default function App() {
+  const [count, setCount] = useState(0)
+
+  const handleClick = () => {
+    setCount((prevCount: number) => prevCount + 1)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{count}</Text>
+      <Button title="Click!" onPress={handleClick}></Button>
+      <StatusBar style="auto"/>
     </View>
   );
 }
